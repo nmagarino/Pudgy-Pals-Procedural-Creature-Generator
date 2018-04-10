5b8739aebd9eb9807c0446a9dd1da983f50027ee
Nick <nmaga@seas.upenn.edu>
2018-04-10 13:53:15 -0400

    Renders just a sphere w/ Lambert

--- src/shaders/raymarch-frag.glsl
+++ src/shaders/raymarch-frag.glsl
@@ -49,104 +49,108 @@ float sphereSDF(vec3 p, float r) {
 		return length(p) - r;
 }
 
-float cubeSDF( vec3 p, float r) {
-	vec3 d = abs(p) - vec3(r, r, r);   
-    float insideDistance = min(max(d.x, max(d.y, d.z)), 0.0);
-    float outsideDistance = length(max(d, 0.0));  
-    return insideDistance + outsideDistance;
-}
+//~~~~~~~~~~~~~~~~~~~~~~~~~CODE FROM ROBOT CONSTRUCTION (SDF REFERENCE)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// float cubeSDF( vec3 p, float r) {
+// 	vec3 d = abs(p) - vec3(r, r, r);   
+//     float insideDistance = min(max(d.x, max(d.y, d.z)), 0.0);
+//     float outsideDistance = length(max(d, 0.0));  
+//     return insideDistance + outsideDistance;
+// }
 
 // h defines the width and height of the cylinder
-float sdCappedCylinder( vec3 p, vec2 h ) {
-  vec2 d = abs(vec2(length(p.xz),p.y)) - h;
-  return min(max(d.x,d.y),0.0) + length(max(d,0.0));
-}
+// float sdCappedCylinder( vec3 p, vec2 h ) {
+//   vec2 d = abs(vec2(length(p.xz),p.y)) - h;
+//   return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+// }
 
-float udRoundBox( vec3 p, vec3 b, float r ) {
-  return length(max(abs(p)-b,0.0))-r;
-}
+// float udRoundBox( vec3 p, vec3 b, float r ) {
+//   return length(max(abs(p)-b,0.0))-r;
+// }
 
-float sdTorus(vec3 p, vec2 t) {
-  vec2 q = vec2(length(p.xz)-t.x,p.y);
-  return length(q)-t.y;
-}
+// float sdTorus(vec3 p, vec2 t) {
+//   vec2 q = vec2(length(p.xz)-t.x,p.y);
+//   return length(q)-t.y;
+// }
 
-float headSDF(vec3 p) {
-	float base = max(sphereSDF(p + vec3(0.0,-.6,0.0), .5), -cubeSDF(p, .6));
-	float eyes = min(sphereSDF(p + vec3(0.25,-0.75,-.33), .1), sphereSDF(p + vec3(-0.25,-0.75,-.33), .1));
-	float antennaes = min(sdCappedCylinder(p + vec3(.4,-1.0,0.2), vec2(.028,.4)), sdCappedCylinder(p + vec3(-.4,-1.0,0.2), vec2(.028,.4)));
-	return min(min(base, eyes), antennaes);
-}
+// float headSDF(vec3 p) {
+// 	float base = max(sphereSDF(p + vec3(0.0,-.6,0.0), .5), -cubeSDF(p, .6));
+// 	float eyes = min(sphereSDF(p + vec3(0.25,-0.75,-.33), .1), sphereSDF(p + vec3(-0.25,-0.75,-.33), .1));
+// 	float antennaes = min(sdCappedCylinder(p + vec3(.4,-1.0,0.2), vec2(.028,.4)), sdCappedCylinder(p + vec3(-.4,-1.0,0.2), vec2(.028,.4)));
+// 	return min(min(base, eyes), antennaes);
+// }
 
-float neckSDF(vec3 p) {
-    float outside = min(min(sdCappedCylinder(p + vec3(0.0,0.0,0.0), vec2(.1, .1)), sdCappedCylinder(p + vec3(0.0,-0.23,0.0), vec2(.1, .1))), 
-		            sdCappedCylinder(p + vec3(0.0,-0.46,0.0), vec2(.1, .1)));
-	float inner = sdCappedCylinder(p + vec3(0.0,-.5,0.0), vec2(.05, .4));
-	return min(inner, outside);
-}
+// float neckSDF(vec3 p) {
+//     float outside = min(min(sdCappedCylinder(p + vec3(0.0,0.0,0.0), vec2(.1, .1)), sdCappedCylinder(p + vec3(0.0,-0.23,0.0), vec2(.1, .1))), 
+// 		            sdCappedCylinder(p + vec3(0.0,-0.46,0.0), vec2(.1, .1)));
+// 	float inner = sdCappedCylinder(p + vec3(0.0,-.5,0.0), vec2(.05, .4));
+// 	return min(inner, outside);
+// }
 
-float bodySDF(vec3 p) {
-	float chest = udRoundBox(p + vec3(0.0, .3, 0.0), vec3(0.27,0.22,0.2), .07);
-	float spine = sdCappedCylinder(p + vec3(0.0,0.74,0.0), vec2(.15, .15));
-	return min(spine, chest);
-}
+// float bodySDF(vec3 p) {
+// 	float chest = udRoundBox(p + vec3(0.0, .3, 0.0), vec3(0.27,0.22,0.2), .07);
+// 	float spine = sdCappedCylinder(p + vec3(0.0,0.74,0.0), vec2(.15, .15));
+// 	return min(spine, chest);
+// }
 
-float pelvisSDF(vec3 p) {
-	return udRoundBox(p + vec3(0.0, 1.1, 0.0), vec3(0.26,0.18,0.17), .07);;
-}
+// float pelvisSDF(vec3 p) {
+// 	return udRoundBox(p + vec3(0.0, 1.1, 0.0), vec3(0.26,0.18,0.17), .07);;
+// }
 
-float shouldersSDF(vec3 p) {
-	return min(udRoundBox(p + vec3(0.3,0.0,0.0), vec3(.1,.1,.23), .095), udRoundBox(p + vec3(-0.3,0.0,0.0) , vec3(.1,.1,.23), .095));
-}
+// float shouldersSDF(vec3 p) {
+// 	return min(udRoundBox(p + vec3(0.3,0.0,0.0), vec3(.1,.1,.23), .095), udRoundBox(p + vec3(-0.3,0.0,0.0) , vec3(.1,.1,.23), .095));
+// }
 
-float handsSDF(vec3 p) {
-	float cutCube = cubeSDF(p + vec3(1.4,-2.42,0.0), .25);
-	float cutCube2 = cubeSDF(p + vec3(-1.4,-2.42,0.0), .25);
-	return min(max(-cutCube, sdTorus(p * rotateMatX(90.0) + vec3(1.4,0.0,-2.06), vec2(0.34,0.08))),
-			   max(-cutCube2, sdTorus(p * rotateMatX(90.0) + vec3(-1.4,0.0,-2.06), vec2(0.34,0.08))));	
-}
+// float handsSDF(vec3 p) {
+// 	float cutCube = cubeSDF(p + vec3(1.4,-2.42,0.0), .25);
+// 	float cutCube2 = cubeSDF(p + vec3(-1.4,-2.42,0.0), .25);
+// 	return min(max(-cutCube, sdTorus(p * rotateMatX(90.0) + vec3(1.4,0.0,-2.06), vec2(0.34,0.08))),
+// 			   max(-cutCube2, sdTorus(p * rotateMatX(90.0) + vec3(-1.4,0.0,-2.06), vec2(0.34,0.08))));	
+// }
 
-float armsSDF(vec3 p) {
-	float biceps =  min(sdCappedCylinder(p * rotateMatZ(90.0) + vec3(0.0,0.6,0.0), vec2(.1,.9)), sdCappedCylinder(p * rotateMatZ(90.0) + vec3(0.0,-0.6,0.0), vec2(.1,.9)));
-    float forearms = min(sdCappedCylinder(p + vec3(1.4,-0.8,0.0), vec2(.1,.9)), sdCappedCylinder(p + vec3(-1.4,-0.8,0.0), vec2(.1,.9)));
+// float armsSDF(vec3 p) {
+// 	float biceps =  min(sdCappedCylinder(p * rotateMatZ(90.0) + vec3(0.0,0.6,0.0), vec2(.1,.9)), sdCappedCylinder(p * rotateMatZ(90.0) + vec3(0.0,-0.6,0.0), vec2(.1,.9)));
+//     float forearms = min(sdCappedCylinder(p + vec3(1.4,-0.8,0.0), vec2(.1,.9)), sdCappedCylinder(p + vec3(-1.4,-0.8,0.0), vec2(.1,.9)));
 
-	return min(handsSDF(p),smin(biceps, forearms, 10.0));
-}
+// 	return min(handsSDF(p),smin(biceps, forearms, 10.0));
+// }
 
-float legsSDF(vec3 p) {
-	return min(sdCappedCylinder(p + vec3(.2,2.0,0.0), vec2(.1, .8)), sdCappedCylinder(p + vec3(-.2,2.0,0.0), vec2(.1, .8)));
-}
+// float legsSDF(vec3 p) {
+// 	return min(sdCappedCylinder(p + vec3(.2,2.0,0.0), vec2(.1, .8)), sdCappedCylinder(p + vec3(-.2,2.0,0.0), vec2(.1, .8)));
+// }
 
-float feetSDF(vec3 p) {
-	float foot1 = udRoundBox(p * rotateMatY(45.0) + vec3(0.2,2.7,-0.33), vec3(0.14,0.05,0.3), 0.06);
-	float foot2 = udRoundBox(p * rotateMatY(-45.0) + vec3(-0.2,2.7,-0.33), vec3(0.14,0.05,0.3), 0.06);
-	return min(foot2, foot1);
-}
+// float feetSDF(vec3 p) {
+// 	float foot1 = udRoundBox(p * rotateMatY(45.0) + vec3(0.2,2.7,-0.33), vec3(0.14,0.05,0.3), 0.06);
+// 	float foot2 = udRoundBox(p * rotateMatY(-45.0) + vec3(-0.2,2.7,-0.33), vec3(0.14,0.05,0.3), 0.06);
+// 	return min(foot2, foot1);
+// }
 
-float upperBodySDF(vec3 p) {
-	float headBod = (min(min(headSDF(rotateMatY(u_Time * 3.6) * p + vec3(0.0,-.1 + sin(u_Time / 2.0)/5.0,0.0) ), neckSDF(p)), smin(bodySDF(p),shouldersSDF(p),10.0) ));
-	float armsToo = armsSDF(p * rotateMatX(-u_Time * 2.1));
-	return min(headBod, armsToo);
-}
+// float upperBodySDF(vec3 p) {
+// 	float headBod = (min(min(headSDF(rotateMatY(u_Time * 3.6) * p + vec3(0.0,-.1 + sin(u_Time / 2.0)/5.0,0.0) ), neckSDF(p)), smin(bodySDF(p),shouldersSDF(p),10.0) ));
+// 	float armsToo = armsSDF(p * rotateMatX(-u_Time * 2.1));
+// 	return min(headBod, armsToo);
+// }
 
-float legsFeet(vec3 p) {
-	return min(legsSDF(p), feetSDF(p));
-}
+// float legsFeet(vec3 p) {
+// 	return min(legsSDF(p), feetSDF(p));
+// }
 
-// Combines elements into one scene
-float wholebodySDF(vec3 p) {
-	vec3 pEdit = p /.15; 
-	float part1 =  upperBodySDF(pEdit * rotateMatY(sin(u_Time / 4.0) * 28.45));
-	float part2 = legsFeet(pEdit * rotateMatX(sin(u_Time / 2.5) * 6.4));
-	return min(min(part1, part2), pelvisSDF(pEdit)) * .15;
-}
+// // Combines elements into one scene
+// float wholebodySDF(vec3 p) {
+// 	vec3 pEdit = p /.15; 
+// 	float part1 =  upperBodySDF(pEdit * rotateMatY(sin(u_Time / 4.0) * 28.45));
+// 	float part2 = legsFeet(pEdit * rotateMatX(sin(u_Time / 2.5) * 6.4));
+// 	return min(min(part1, part2), pelvisSDF(pEdit)) * .15;
+// }
+//~~~~~~~~~~~~~~~~~~~~~~~~~CODE FROM ROBOT CONSTRUCTION (SDF REFERENCE)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
+
+// OVERALL SCENE SDF -- rotates about z-axis (turn-table style)
 float sceneSDF(vec3 p) {
-	vec3 c = vec3(1.01, 1.01, 0.0);
-	vec3 q = mod(p,c)-0.5*c;
-	return wholebodySDF(q);
+	return sphereSDF(p * rotateMatZ(u_Time), .2);
 }
 
+
+//~~~~~~~~~~~~~~~~~~~~ACTUAL RAY MARCHING STUFF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 float march(vec3 rayOrigin, vec3 direction) {
 	float dist = MIN_DIST;
 	for(int i = 0; i < MAX_STEPS; i++) {
@@ -180,9 +184,7 @@ mat4 viewMatrix(vec3 eye, vec3 center, vec3 up) {
         vec4(-f, 0.0),
         vec4(0.0, 0.0, 0.0, 1)
     );
-}
-
-
+} 
 
 void main() {
 	
@@ -202,7 +204,7 @@ void main() {
 		return;
 	}
 
-	// Colors geometry
+	// Colors geometry with Lambert Shader
 	 vec3 p = eye + distance * dir;
 	 vec3 lightVec = vec3(17.0,40.0,50.0) - p;
 	 float diffuseTerm = dot(normalize(normal(p)), normalize(lightVec));
@@ -210,13 +212,5 @@ void main() {
 	 float ambientTerm = 0.2;
 	 float lightIntensity = diffuseTerm + ambientTerm;
 
-	 if(dot(dir, normal(p)) < mix(45.0, 44.0, max(0.0, dot(normal(p), lightVec)))) {
-		 out_Col = vec4( vec3(1.0,1.0,1.0) * vec3(0.0,0.0,0.0) ,1.0);
-		 return;
-	 }
-
-	 if(1.0 * max(0.0, dot(normal(p), lightVec)) <= 60.8) {
-		 out_Col = vec4( vec3(1.0,0.0,0.0) * vec3(1.0,1.0,1.0), 1.0);
-		 return;
-	 }
+	 out_Col = vec4(vec3(1.0,0.0,0.0) * lightIntensity, 1.0) ;
 }
